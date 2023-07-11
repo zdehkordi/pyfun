76e74ee20c68631a048af4c867250e0db349be50
zdehkordi <zac.dehkordi@gmail.com>
2023-07-11 11:15:21 -0400

    implement some methods

--- pyfun/arrays/vector.py
+++ pyfun/arrays/vector.py
@@ -6,7 +6,7 @@ T = TypeVar('T')
 class array(Generic[T]):
     """dum arrays!"""
     def __init__(self, cls: type[T], length: int) -> None:
-        self.__data: list[T] = [cls()] * length
+        self.__data: list[T] = [None] * length
         self.length = length
 
     def __getitem__(self, key: int) -> T:
@@ -24,8 +24,17 @@ class array(Generic[T]):
 
 class vector(Generic[T]):
     """generic vectors, aka 'list'"""
-    def __init__(self) -> None:
-        pass
+    def __init__(self, cls: type[T], size: int = 1) -> None:
+        self.__data = array(cls, size)
+
+    def add(self, val: T) -> None:
+        self.__data[0] = val
 
     def size(self) -> int:
-        return 0
+        return sum(1 for i in range(self.__data.length) if self.__data[i] is not None)
+    
+    def capacity(self) -> int:
+        return self.__data.length
+
+    def is_empty(self) -> bool:
+        return all(self.__data[i] is None for i in range(self.__data.length))
--- tests/arrays/test_vector.py
+++ tests/arrays/test_vector.py
@@ -13,7 +13,7 @@ def test_get_length_of_array():
 
 def test_get_and_set_val_of_array():
     a = array(int, 1)
-    assert a[0] == 0
+    assert a[0] == None
     a[0] = 1
     assert a[0] == 1
 
@@ -23,9 +23,31 @@ def test_make_with_vals():
     assert a[2] == 3
 
 def test_can_make_vector():
-    v = vector()
+    v = vector(int)
 
-def test_has_set_size():
-    v = vector()
+def test_has_size():
+    v = vector(int)
 
     assert v.size() == 0
+
+def test_has_size_with_elements():
+    v = vector(int)
+
+    v.add(1)
+
+    assert v.size() == 1
+
+def test_capactiy():
+    v = vector(int, 1)
+    assert v.capacity() == 1
+
+    v = vector(int, 32)
+    assert v.capacity() == 32
+
+def test_is_empty():
+    v = vector(int)
+    assert v.is_empty() == True
+
+    v.add(1)
+
+    assert v.is_empty() == False
