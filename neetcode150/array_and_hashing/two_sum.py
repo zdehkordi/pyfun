07d5644ca5f09ea08712e889c19963a8d0d9df0e
zdehkordi <zac.dehkordi@gmail.com>
2023-07-27 15:58:06 -0400

    two sum

--- neetcode150/array_and_hashing/two_sum.py
+++ neetcode150/array_and_hashing/two_sum.py
@@ -1,7 +1,33 @@
 import pytest
 
-def func():
-    pass
+def func(nums: list[int], target: int) -> list[int]:
+    m = {}
 
-def test_():
-    assert True
+    for i in range(len(nums)):
+        c = target - nums[i]
+        if c in m:
+            return [m[c], i]
+
+        m[nums[i]] = i 
+
+    return []
+
+def test_base():
+    nums, target = [1,2], 5
+    assert func(nums, target) == []
+
+def test_ez():
+    nums, target = [1,2], 3
+    assert func(nums, target) == [0,1]
+
+def test_ex1():
+    nums, target = [2,7,11,15], 9
+    assert func(nums, target) == [0,1]
+
+def test_ex2():
+    nums, target = [3,2,4], 6
+    assert func(nums, target) == [1,2]
+
+def test_ex3():
+    nums, target = [3,3], 6
+    assert func(nums, target) == [0,1]
