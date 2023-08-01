4bc0e23a9159acfcdc9fd653b158875d5f4d0c41
zdehkordi <zac.dehkordi@gmail.com>
2023-08-01 14:02:11 -0400

    three sum is hard

--- neetcode150/twopointers/3sum.py
+++ neetcode150/twopointers/3sum.py
@@ -10,15 +10,35 @@ def func(nums: list[int]) -> list[list[int]]:
     return [list(x) for x in res]
 
 def func(nums: list[int]) -> list[list[int]]:
-    s = set(nums)
-    res = []
-    for i in range(len(nums)-1):
-        for j in range(i+1, len(nums)-1):
-            if -nums[i]-nums[j] in s:
-                toad = sorted([-nums[i]-nums[j], nums[j], nums[i]])
-                if toad not in res:
-                    res.append(toad)
-    return res
+    nums.sort()
+    ans = []
+
+    for i, a in enumerate(nums):
+        if a > 0: break
+        if i > 0 and a == nums[i-1]: continue
+
+        l, r = i+1, len(nums)-1
+
+        while l < r:
+            diff = a + nums[l] + nums[r]
+            if diff  == 0:
+                ans.append([a, nums[l], nums[r]])
+                l += 1
+                r -= 1
+
+                while nums[l] == nums[l-1] and l < r:
+                    l += 1
+
+            elif diff > 0:
+                r -= 1
+            else:
+                l += 1
+
+        i += 1
+
+    return ans
+        
+            
 
 
 
